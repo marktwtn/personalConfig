89bdb039e5d105c16bc84de6e24950d6383de65e
marktwtn <marktwtn@gmail.com>
2017-08-15 16:59:54 +0800

    Make user decide to replace the file or not

--- config.sh
+++ config.sh
@@ -8,9 +8,32 @@ fishFile=${home}/.config/fish/config.fish
 
 # Check whether the file exists or not.
 if [ -f ${fishFile} ]; then
-    # If it exists, ask the user to replace it or not.
-    cat ${fishFile}
+    # If it exists, print the content of the files.
+    echo ""
+    echo "The content in your computer:"
+    echo ""
+    echo "********************"
+    cat  ${fishFile}
+    echo "********************"
+    echo ""
+    echo "The content in the repository:"
+    echo ""
+    echo "********************"
+    cat  ./config.fish
+    echo "********************"
+    echo ""
+    # Ask the user to replace it or not.
+    read -p "Replace the file in your computer or not (y/n): " decision
+    while [ ${decision} != 'y' ] && [ ${decision} != 'n' ]
+    do
+        echo    "Please enter y or n"
+        read -p "Replace the file in your computer or not (y/n): " decision
+    done
+    # Do the things according to the answer of the user
+    if [ ${decision} == 'y' ]; then
+        cp ./config.fish ${fishFile}
+    fi
 else
-    # If it does not exist, add the file.
+    # If it does not exist, copy the file.
     cp ./config.fish ${fishFile}
 fi
