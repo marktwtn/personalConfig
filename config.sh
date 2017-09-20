a02c41cd1b0979efbd3153ef9361c74f8a002c61
marktwtn <marktwtn@gmail.com>
2017-09-20 23:03:21 +0800

    Loop through the configuration files

--- config.sh
+++ config.sh
@@ -3,37 +3,52 @@
 set -e
 
 # Variables
+# home directory
 home=~
-fishFile=${home}/.config/fish/config.fish
+# File in repository
+fishFile=config.fish
+vimFile=vimrc
+# File in local computer
+fishLocFile=${home}/.config/fish/${fishFile}
+vimLocFile=${home}/.${vimFile}
+# File array
+repoList=(${fishFile} ${vimFile})
+locList=(${fishLocFile} ${vimLocFile})
+# Array length
+len=${#repoList[*]}
 
+# Shell script
 # Check whether the file exists or not.
-if [ -f ${fishFile} ]; then
-    # If it exists, print the content of the files.
-    echo ""
-    echo "The content in your computer:"
-    echo ""
-    echo "********************"
-    cat  ${fishFile}
-    echo "********************"
-    echo ""
-    echo "The content in the repository:"
-    echo ""
-    echo "********************"
-    cat  ./config.fish
-    echo "********************"
-    echo ""
-    # Ask the user to replace it or not.
-    read -p "Replace the file in your computer or not (y/n): " decision
-    while [ ${decision} != 'y' ] && [ ${decision} != 'n' ]
-    do
-        echo    "Please enter y or n"
+for idx in $( seq 0 $(( ${len}-1 )) )
+do
+    if [ -f ${locList[${idx}]} ]; then
+        # If it exists, print the content of the files.
+        echo ""
+        echo "The content in your computer:"
+        echo ""
+        echo "********************"
+        cat  ${locList[${idx}]}
+        echo "********************"
+        echo ""
+        echo "The content in the repository:"
+        echo ""
+        echo "********************"
+        cat  ${repoList[${idx}]}
+        echo "********************"
+        echo ""
+        # Ask the user to replace it or not.
         read -p "Replace the file in your computer or not (y/n): " decision
-    done
-    # Do the things according to the answer of the user
-    if [ ${decision} == 'y' ]; then
-        cp ./config.fish ${fishFile}
+        while [ ${decision} != 'y' ] && [ ${decision} != 'n' ]
+        do
+            echo    "Please enter y or n"
+            read -p "Replace the file in your computer or not (y/n): " decision
+        done
+        # Do the things according to the answer of the user
+        if [ ${decision} == 'y' ]; then
+            cp ${repoList[${idx}]} ${locList[${idx}]}
+        fi
+    else
+        # If it does not exist, copy the file.
+        cp ${repoList[${idx}]} ${locList[${idx}]}
     fi
-else
-    # If it does not exist, copy the file.
-    cp ./config.fish ${fishFile}
-fi
+done
